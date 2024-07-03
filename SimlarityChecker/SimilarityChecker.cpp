0226bd623e8f64c04be0bead9a954745317c56fe
Myeong Woo Kim <mkim0628@gmail.com>
2024-07-04 08:46:58 +0900

    legnth check

---
+++ SimlarityChecker/SimilarityChecker.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class SimilarityChecker {
+public:
+	const int LENGTH_SCORE_MAX = 60;
+	const int LENGTH_SCORE_MIN = 0;
+	
+	int compareLength(string str1, string str2) {
+		if (str1.length() > str2.length()) {
+			longStrLength = str1.length();
+			shortStrLength = str2.length();
+		}
+		else {
+			longStrLength = str2.length();
+			shortStrLength = str1.length();
+		}
+
+		if (shortStrLength == longStrLength)
+			return LENGTH_SCORE_MAX;
+		if (longStrLength >= shortStrLength * 2)
+			return LENGTH_SCORE_MIN;
+		
+		return (float)(2 * shortStrLength - longStrLength) / (float)shortStrLength * LENGTH_SCORE_MAX;
+		
+	}
+private:
+	int shortStrLength;
+	int longStrLength;
+};
