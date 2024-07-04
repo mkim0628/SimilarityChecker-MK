1cec4092740e053dda9c20573845e126c2d67428
Myeong Woo Kim <mkim0628@gmail.com>
2024-07-04 09:19:31 +0900

    [refactor] Extract a function to calculate short string length and long stringn length

--- SimlarityChecker/SimilarityChecker.cpp
+++ SimlarityChecker/SimilarityChecker.cpp
@@ -7,6 +7,15 @@ public:
 	const int LENGTH_SCORE_MIN = 0;
 	
 	int compareLength(string str1, string str2) {
+		calShortLength(str1, str2);
+		if (shortStrLength == longStrLength)
+			return LENGTH_SCORE_MAX;
+		if (longStrLength >= shortStrLength * 2)
+			return LENGTH_SCORE_MIN;
+		return (float)(2 * shortStrLength - longStrLength) / (float)shortStrLength * LENGTH_SCORE_MAX;
+	}
+private:
+	void calShortLength(string str1, string str2) {
 		if (str1.length() > str2.length()) {
 			longStrLength = str1.length();
 			shortStrLength = str2.length();
@@ -15,16 +24,8 @@ public:
 			longStrLength = str2.length();
 			shortStrLength = str1.length();
 		}
-
-		if (shortStrLength == longStrLength)
-			return LENGTH_SCORE_MAX;
-		if (longStrLength >= shortStrLength * 2)
-			return LENGTH_SCORE_MIN;
-		
-		return (float)(2 * shortStrLength - longStrLength) / (float)shortStrLength * LENGTH_SCORE_MAX;
-		
 	}
-private:
 	int shortStrLength;
 	int longStrLength;
+
 };
